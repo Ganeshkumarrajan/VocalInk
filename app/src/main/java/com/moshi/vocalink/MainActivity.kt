18b8fac2916ed15fa2d672c4788683bc34c865aa
ganeshkumarrajan <ganeshapikey@gmail.com>
2025-07-11 16:35:39 +0200

    initial commit

--- app/src/main/java/com/moshi/vocalink/MainActivity.kt
+++ app/src/main/java/com/moshi/vocalink/MainActivity.kt
@@ -1,19 +1,10 @@
 package com.moshi.vocalink
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.vocalink.ui.theme.VocalInkTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,27 +12,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val voiceManager = VoiceToTextManager(this)
         setContent {
             MaterialTheme {
                 VoiceToTextScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VocalInkTheme {
-        Greeting("Android")
     }
 }
